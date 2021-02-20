fbab5eb6239a6121016cc9d60d667864f0f32755
Rashmi_CM <rashmi.cm@powerschool.com>
2021-02-20 23:00:27 +0530

    adding all files

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+k
+;src/main/java/com/springBoot/Controller/BookController.java,0\7\07499b1c6e027b6c5fbc2a2dce0f1aa136b10acd
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+a
+1src/main/java/com/springBoot/BookApplication.java,e\1\e11675b54943707bb37dad3310e30fd071feba98
+X
+(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+;
+gradlew.bat,2\a\2a45a911a8f1836b0b6c5b758962572012d8f8c3
---
+++ src/main/java/com/springBoot/Controller/BookController.java
@@ -0,0 +1,82 @@
+package com.springBoot.Controller;
+
+import com.springBoot.Repository.BookRepository;
+import com.springBoot.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Rashmi.CM
+ * @since 30-12-2020
+ */
+@RestController
+public class BookController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "Landing Page";
+    }
+
+    @GetMapping("/check1")
+    public String checkPage(){
+        return "Landing Page of checkPage";
+    }
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/getAllBooks")
+    public List<Book> getAllBooks()
+    {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/getBookById/{id}")
+    public Optional<Book> getBookById(@PathVariable(value="id") Long id)
+    {
+        return Optional.of(bookRepository.findById(id).orElse(new Book(id, "", "", "","Incorrect Id:Book not found")));
+    }
+
+    @PostMapping("/addBook")
+    public Book addBook(@RequestBody Book book)
+    {
+        book.setBookStatus("Book Saved");
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/updateBook/{id}")
+    public Book updateBook(@PathVariable(value="id") Long id, @RequestBody Book bookDetails)
+    {
+        Optional<Book> book = bookRepository.findById(id);
+        if(book.isPresent()) {
+            Book book_new = book.get();
+            book_new.setAuthor_name(bookDetails.getAuthor_name());
+            book_new.setBook_name(bookDetails.getBook_name());
+            book_new.setIsbn(bookDetails.getIsbn());
+            book_new.setBookStatus(bookDetails.getBook_name() + " ," + bookDetails.getIsbn() + " :Book details updated");
+            return bookRepository.save(book_new);
+        }
+        else{
+            bookDetails.setBookStatus("Unable to update BookDetails due to incorrect BookId");
+            bookDetails.setId(id);
+            return bookDetails;
+        }
+    }
+
+    @DeleteMapping("/deleteBook/{id}")
+    public Book deleteBook(@PathVariable(value="id") Long id)
+    {
+        Optional<Book> book=bookRepository.findById(id);
+        if(book.isPresent()) {
+            Book bookDetails = book.get();
+            bookDetails.setBookStatus("Deleted Book with Id:" + id + " Name:" +  bookDetails.getBook_name());
+            bookRepository.delete(bookDetails);
+            return bookDetails;
+        }
+        return new Book(new Long(-1), "", "", "","Incorrect Id input");
+    }
+
+}
